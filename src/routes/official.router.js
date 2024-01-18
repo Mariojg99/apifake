1ad614bc3b6110197c024f83302e22f2a6f560cc
Jose Mario <jmgulfo@softcaribbean.com>
2024-01-18 17:38:14 -0500

    se agrega petición get

--- src/routes/official.router.js
+++ src/routes/official.router.js
@@ -4,5 +4,18 @@ const officialController = require('../controllers/official.controller');
 
 router
     .post('/getByIdNumber', officialController.getByIdNumber)
+    .get("/", (req, res) => {
+        const htmlResponse = `
+          <html>
+            <head>
+              <title>NodeJs y Express en Vercel</title>
+            </head>
+            <body>
+              <h1>Soy un proyecto Back end en vercel</h1>
+            </body>
+          </html>
+        `;
+        res.send(htmlResponse);
+      });
 
 module.exports = router;
